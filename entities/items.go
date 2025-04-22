53f852456e6989da66fb0b1b53be0a7047ea016a
minh.vd <minhvd.1111@gmail.com>
2025-04-22 08:59:06 +0700

    S3.2.7: merge potion & gold to items

--- entities/items.go
+++ entities/items.go
@@ -1,5 +1,14 @@
 package entities
 
+type Potion struct {
+	X, Y   int
+	Symbol rune
+}
+
+func NewPotion(x, y int) Potion {
+	return Potion{X: x, Y: y, Symbol: '!'}
+}
+
 type Gold struct {
 	X, Y   int
 	Symbol rune
